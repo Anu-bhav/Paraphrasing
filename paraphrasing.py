fd5782541c85ba6d12c628471741aa2f65ce5142
Anubhav <myster.anubhav@gmail.com>
2020-01-30 21:45:37 +0400

    Login Info Added

--- paraphrasing.py
+++ paraphrasing.py
@@ -4,6 +4,11 @@ Author: Anubhav
 Date: 29.01.2020
 
 The program will take a processed textfile as input, the file should have every sentence in a new line
+
+Login Info to get cookies
+https://quillbot.com/
+Email: prinusam.roy.9e@819760.com
+Password: password
 """
 import os.path
 from json import loads
@@ -96,16 +101,19 @@ def paraphrasor(url, session):
     """
     # Cookies are configurable
     cookies = {
-        "__cfduid": "deaff692a20199c189d2fc18b1e1b81ce1578851672;",
-        "_ga": "GA1.2.1058973520.1578851672;",
-        "amplitude_id_6e403e775d1f5921fdc52daf8f866c66quillbot.com": "eyJkZXZpY2VJZCI6ImM5YmM2NDk2LWFiMDAtNDJmNC1hNDY1LTljMjNmNTUxZWI2NVIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU3ODg1MTY3MjgwOSwibGFzdEV2ZW50VGltZSI6MTU3ODg1MTY3MjgwOSwiZXZlbnRJZCI6MCwiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjB9;",
-        "sessID": "c1d8a534715fec4e;",
-        "_gid": "GA1.2.1411973136.1580293871;",
-        "premium": "true;",
-        "userIDToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1OTc0MmQyNjlhY2IzNWZiNjU3YzBjNGRkMmM3YjcyYWEzMTRiNTAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcGFyYXBocmFzZXItNDcyYzEiLCJhdWQiOiJwYXJhcGhyYXNlci00NzJjMSIsImF1dGhfdGltZSI6MTU4MDI5NTk4MSwidXNlcl9pZCI6InRVeUZJRUZXd2dXSjl2RUszNzlXUEdSRnBxeTEiLCJzdWIiOiJ0VXlGSUVGV3dnV0o5dkVLMzc5V1BHUkZwcXkxIiwiaWF0IjoxNTgwMjk1OTgyLCJleHAiOjE1ODAyOTk1ODIsImVtYWlsIjoiMWVyLm1la25lczIwMTRAY3lmdS5pY3UiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyIxZXIubWVrbmVzMjAxNEBjeWZ1LmljdSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.lWr63CbAj168AbAFoDjlR5V-ulN187_hbtHYqOcf461_dVOqa6zuhhoSn1LjrZ6O7aqIJC1lgNN0-1pXl_rGDZXSkG_YXoxkAlKdC1KzM-Gck2djEQ6Zj7VCbsimdVMZWPAVjGq6OYrbm9i9FixXmMz4gs2yYIkeulfm-y8zlt-oWpGKf2nrEEdk7WMeLOlQc6vHrerTeO0weuNydsWWw4ndoGmfFrimRc5ehc2nONC7--AhAHgeggnEfdbwM9k8UjVU0UJilNVn5A87OdLJ7GFepvE4u0xVOQjoeFGOO0y7ZrqRLATOqxbuKt0XvquyLcu8uYlfgrx73ZtlX13fvw;",
-        "connect.sid": "s%3AUWl0su0IriH7t-5FtJMaanB0OnbrycOY.QHFucYo4RdOeHKk5EjkaWiZamDuFIOcD3AozTTOtHCo;",
+        "__cfduid": "d90a7ce3fa0772a83274fe34ad8c58a9f1580405470;",
+        "_ga": "GA1.2.1926625509.1580405472;",
+        "_gid": "GA1.2.1282073988.1580405472;",
+        "sessID": "b0f5b816e094bfea;",
+        "connect.sid": "s%3AwdYjihyTxyB5J_BoJ3_n1dVy3P5kGPUQ.IYeruJ47DcK7AIqj%2BjbKlkZkOmDeV1BccWwf%2BYTKKk4;",
         "authenticated": "true;",
-        "quid": "tUyFIEFWwgWJ9vEK379WPGRFpqy1"
+        "premium": "true;",
+        "quid": "RqEHTeLDBPcZTsI1rYo5Mfcu3d02;",
+        "__stripe_sid": "28dae5a8-98b1-4420-9ade-fc7fe1be84a1;",
+        "__stripe_mid": "3b243369-c00a-49f1-b007-c3d49ad4e2b6;",
+        "amplitude_id_6e403e775d1f5921fdc52daf8f866c66quillbot.com": "eyJkZXZpY2VJZCI6IjgzYzAzZTc0LThhMzktNDAwYS1hNDA5LWI4MzZkNDhhM2FiZFIiLCJ1c2VySWQiOiJwcmludXNhbS5yb3kuOWVAODE5NzYwLmNvbSIsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU4MDQwNTQ3MzMxNCwibGFzdEV2ZW50VGltZSI6MTU4MDQwNTYyMDEzNSwiZXZlbnRJZCI6MywiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjN9;",
+        "_gat": "1;",
+        "userIDToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1OTc0MmQyNjlhY2IzNWZiNjU3YzBjNGRkMmM3YjcyYWEzMTRiNTAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcGFyYXBocmFzZXItNDcyYzEiLCJhdWQiOiJwYXJhcGhyYXNlci00NzJjMSIsImF1dGhfdGltZSI6MTU4MDQwNTUyMCwidXNlcl9pZCI6IlJxRUhUZUxEQlBjWlRzSTFyWW81TWZjdTNkMDIiLCJzdWIiOiJScUVIVGVMREJQY1pUc0kxcllvNU1mY3UzZDAyIiwiaWF0IjoxNTgwNDA1NjIxLCJleHAiOjE1ODA0MDkyMjEsImVtYWlsIjoicHJpbnVzYW0ucm95LjllQDgxOTc2MC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJwcmludXNhbS5yb3kuOWVAODE5NzYwLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.dhiWkSZFqb6VkyJunASN9sgHdNDe5BffVaB8iX7qw479aB-nzu8L5psbn3ev2nw3r2raYEVD-1jHYXic-GyR_LO6pPWfqbvrcCnkZDkUVLiAIVQjB09CSFh5bqL_L7Zp6imLzgT6Tk6mk162FyZYD9DxLYy7KpsyJEhc41TQX-66hWkce2Zghl0VTjWUcF-Jszo66nAC1aAUHMDOA8umMAewlVVKf05MlRKY-Gs9xuMWenr3-gUV4pgCUk5acApqe84K7uJPIXEbmYH_35o7X0f1hxB8KbcbRUTEFwA-Clu8SnZu3dbblGf7YbNHeWmZYeLB7jGHugDQp5GlAFYojg"
     }
 
     req = session.get(url, cookies=cookies)
