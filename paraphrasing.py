a0931410cbb331ad918f9da8c9c5ac22c810a9b9
Anubhav <myster.anubhav@gmail.com>
2020-02-10 18:24:07 +0400

    remove extra line in docx

--- paraphrasing.py
+++ paraphrasing.py
@@ -53,8 +53,8 @@ def get_parameterized_url(text):
     """
     url_encoded_text = quote(text)
     autoflip = "true"
-    fthresh = "true"
-    strength = "4"
+    fthresh = "4"
+    strength = "5"
     wikify = "9"
     url = API_URL + PARAMS.format(url_encoded_text, strength, autoflip, wikify, fthresh)
     return url
@@ -101,19 +101,18 @@ def paraphrasor(url, session):
     """
     # Cookies are configurable
     cookies = {
-        "__cfduid": "d90a7ce3fa0772a83274fe34ad8c58a9f1580405470;",
-        "_ga": "GA1.2.1926625509.1580405472;",
-        "_gid": "GA1.2.1282073988.1580405472;",
-        "sessID": "b0f5b816e094bfea;",
-        "connect.sid": "s%3AwdYjihyTxyB5J_BoJ3_n1dVy3P5kGPUQ.IYeruJ47DcK7AIqj%2BjbKlkZkOmDeV1BccWwf%2BYTKKk4;",
+        "__cfduid": "dc0840a2a6813fdf6a4cdc61e6f9fccde1581242299;",
+        "_ga": "GA1.2.2004764009.1581242301;",
+        "_gid": "GA1.2.19653033.1581242301;",
+        "connect.sid": "s%3Ahe1zGWUi5utvCE_O-PEh3Rb3moDifGYy.1kQ1L8iIO5BaBTtyi9re8jFZIG2YBmMO14fbTwxGYto;",
+        "userIDToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzZTllYThmNzNkZWExMTRkZWI5YTY0OTcxZDJhMjkzN2QwYzY3YWEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcGFyYXBocmFzZXItNDcyYzEiLCJhdWQiOiJwYXJhcGhyYXNlci00NzJjMSIsImF1dGhfdGltZSI6MTU4MTI0MjMxOCwidXNlcl9pZCI6IlJxRUhUZUxEQlBjWlRzSTFyWW81TWZjdTNkMDIiLCJzdWIiOiJScUVIVGVMREJQY1pUc0kxcllvNU1mY3UzZDAyIiwiaWF0IjoxNTgxMjQyMzIwLCJleHAiOjE1ODEyNDU5MjAsImVtYWlsIjoicHJpbnVzYW0ucm95LjllQDgxOTc2MC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJwcmludXNhbS5yb3kuOWVAODE5NzYwLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.dBIarlzbCSdZ4GdOoUy8sB_8jzIGdccj3oz0L6GFQkI5yCUWhgJs5XXMESV2QM4QamPm5MPstqLV2Tc1CGRijADBixMS5ZwqVk7twLTmriqZVYwrFEEhtsZYi3VwBLwcetRt-9jrBnzrFK0chus8_g7tf--HPTqn4VN4dXzu70GGMz6nbUVf7_dOmlJReUUWw5Ff2qorMMOYyh6_UhdrCQBcKz3bX_5WJBUPr5vR4cxDyIdAft9Idizi8-5tMJ2UwlTJ-D0DQviN2lUWJ_kJaKoL9dAuQgkryeGBAGLzxIdZlhCPZpaaro8t32QpYgrUWTu0j4PjiRZbM8hu0NH6Ag;",
         "authenticated": "true;",
-        "premium": "true;",
+        "premium": "false;",
         "quid": "RqEHTeLDBPcZTsI1rYo5Mfcu3d02;",
-        "__stripe_sid": "28dae5a8-98b1-4420-9ade-fc7fe1be84a1;",
-        "__stripe_mid": "3b243369-c00a-49f1-b007-c3d49ad4e2b6;",
-        "amplitude_id_6e403e775d1f5921fdc52daf8f866c66quillbot.com": "eyJkZXZpY2VJZCI6IjgzYzAzZTc0LThhMzktNDAwYS1hNDA5LWI4MzZkNDhhM2FiZFIiLCJ1c2VySWQiOiJwcmludXNhbS5yb3kuOWVAODE5NzYwLmNvbSIsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU4MDQwNTQ3MzMxNCwibGFzdEV2ZW50VGltZSI6MTU4MDQwNTYyMDEzNSwiZXZlbnRJZCI6MywiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjN9;",
+        "REGION": "uk;",
+        "amplitude_id_6e403e775d1f5921fdc52daf8f866c66quillbot.com": "eyJkZXZpY2VJZCI6IjgwM2YwMGQ4LTc1ODgtNDBmMy1iMzczLWVkM2NkZDVjYTU5OVIiLCJ1c2VySWQiOiJwcmludXNhbS5yb3kuOWVAODE5NzYwLmNvbSIsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU4MTI2MTc0MDkxOCwibGFzdEV2ZW50VGltZSI6MTU4MTI2MzcxNTkzNCwiZXZlbnRJZCI6MzAxLCJpZGVudGlmeUlkIjowLCJzZXF1ZW5jZU51bWJlciI6MzAxfQ==;",
         "_gat": "1;",
-        "userIDToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1OTc0MmQyNjlhY2IzNWZiNjU3YzBjNGRkMmM3YjcyYWEzMTRiNTAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcGFyYXBocmFzZXItNDcyYzEiLCJhdWQiOiJwYXJhcGhyYXNlci00NzJjMSIsImF1dGhfdGltZSI6MTU4MDQwNTUyMCwidXNlcl9pZCI6IlJxRUhUZUxEQlBjWlRzSTFyWW81TWZjdTNkMDIiLCJzdWIiOiJScUVIVGVMREJQY1pUc0kxcllvNU1mY3UzZDAyIiwiaWF0IjoxNTgwNDA1NjIxLCJleHAiOjE1ODA0MDkyMjEsImVtYWlsIjoicHJpbnVzYW0ucm95LjllQDgxOTc2MC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJwcmludXNhbS5yb3kuOWVAODE5NzYwLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.dhiWkSZFqb6VkyJunASN9sgHdNDe5BffVaB8iX7qw479aB-nzu8L5psbn3ev2nw3r2raYEVD-1jHYXic-GyR_LO6pPWfqbvrcCnkZDkUVLiAIVQjB09CSFh5bqL_L7Zp6imLzgT6Tk6mk162FyZYD9DxLYy7KpsyJEhc41TQX-66hWkce2Zghl0VTjWUcF-Jszo66nAC1aAUHMDOA8umMAewlVVKf05MlRKY-Gs9xuMWenr3-gUV4pgCUk5acApqe84K7uJPIXEbmYH_35o7X0f1hxB8KbcbRUTEFwA-Clu8SnZu3dbblGf7YbNHeWmZYeLB7jGHugDQp5GlAFYojg"
+        "sessID": "ff9e579bb9ef1a98"
     }
 
     req = session.get(url, cookies=cookies)
@@ -136,14 +135,14 @@ def paraphrasor(url, session):
 
         print("_" * 90, "\n")
         DOC.add_paragraph("_" * 90)
-        DOC.add_paragraph()
+        # DOC.add_paragraph()
 
         for text in texts:
             print(text, end)
             DOC.add_paragraph(text)
 
         print("#" * 90)
-        DOC.add_paragraph("#" * 90)
+        DOC.add_paragraph("#" * 63)
 
 
 def main():
